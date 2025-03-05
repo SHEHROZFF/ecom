67e0945e042747367d54069721cd96837212bdf5
shez222 <bilal.shehroz420@gmail.com>
2025-03-05 07:55:44 +0500

    chnage

--- Backend/index.js
+++ Backend/index.js
@@ -17,6 +17,8 @@ const courseRoutes = require('./routes/courseRoutes');
 const adRoutes = require('./routes/adRoutes');
 const enrollmentRoutes = require('./routes/enrollmentRoutes');
 const policiesRoutes = require('./routes/policiesRoutes');
+const configRoutes = require('./routes/config');
+
 
 // const stripe = require('stripe')('sk_test_51OXlAIAZK57wNYnQQluuPOe6YHwpKCs2dZfKLaEe7Ye67OObYR3Hes3i0Vjo1yp450mlVWQ9ufvWWYYymF1mc33R00GwSCgwFi');
 
@@ -85,6 +87,8 @@ app.use('/api/courses', courseRoutes); // Mount the courses routes
 app.use('/api/ads', adRoutes);
 app.use('/api/enrollments', enrollmentRoutes);
 app.use('/api/policies', policiesRoutes);
+app.use('/api/config', configRoutes);
+
 
 // Error handling middleware (should be last piece of middleware)
 app.use(errorHandler);
